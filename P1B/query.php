fe698688fe984ed8fa8f1b44ad0ced15aacf7a2c
AllenShieh <yyy88xxx@163.com>
2018-04-15 12:49:40 -0700

    update

--- P1B/query.php
+++ P1B/query.php
@@ -5,15 +5,30 @@
 <h1>Movie Database Quesry Interface</h1>
 
 <form action="query.php" method="post">
-<input type="text" name="query">
+<!-- <input type="text" name="query"> -->
+<textarea rows='6' cols='40' name="query"></textarea></br>
 <input type="submit" value="Get it done!">
 </form>
 
 <h1>Results</h1>
 
 <?php
+$query = $_POST["query"];
+echo $query.'<br>';
 
-
+$mysqli = new mysqli("localhost", "cs143", "", "CS143");
+$result = $mysqli->query("SELECT * FROM Movie WHERE id=100");
+while($row = $result->fetch_row())
+{
+  //print_r($row);
+  foreach($row as $val)
+  {
+    echo $val.' ';
+  }
+}
+//$row = $result->fetch_assoc();
+//echo htmlentities($row);
+//echo $query;
 
 ?>
 
--- P1B/reset.sql
+++ P1B/reset.sql
@@ -1,4 +1,3 @@
-
 truncate Movie;
 
 truncate Actor;
