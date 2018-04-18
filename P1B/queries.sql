47c3a9d128f2391623ed61d06f23e9cabefb40fb
AllenShieh <yyy88xxx@163.com>
2018-04-17 18:13:51 -0700

    table done

--- P1B/queries.sql
+++ P1B/queries.sql
@@ -1,15 +1,20 @@
-/*
-select concat(first, ' ', last)
+select concat(first, ' ', last) as actorname
 from Actor inner join MovieActor on Actor.id=MovieActor.aid inner join Movie on MovieActor.mid=Movie.id
 where Movie.title='Die Another Day'
 ;
-*/
 
-/*
+
 select count(*) from
 (
   select aid from MovieActor
   group by aid having count(distinct mid)>1
 ) as t
 ;
-*/
+
+-- query the title of the movie with the most actors
+select title, count(aid) as numberofactor
+from MovieActor join Movie on MovieActor.mid=Movie.id
+group by mid
+order by count(aid) desc
+limit 1
+;
--- P1B/query.php
+++ P1B/query.php
@@ -6,7 +6,7 @@
 
 <form action="query.php" method="post">
 <!-- <input type="text" name="query"> -->
-<textarea rows='6' cols='40' name="query"></textarea></br>
+<textarea rows='6' cols='40' name="query"><?php echo $_POST["query"];?></textarea></br>
 <input type="submit" value="Get it done!">
 </form>
 
@@ -14,22 +14,51 @@
 
 <?php
 $query = $_POST["query"];
-echo $query.'<br>';
+echo '<h3>Input query is:</h3>'.$query.'<br>';
 
-$mysqli = new mysqli("localhost", "cs143", "", "CS143");
-$result = $mysqli->query($query);
-while($row = $result->fetch_row())
-{
+echo '<h3>The results of the query are:</h3>';
+
+try{
+  $mysqli = new mysqli("localhost", "cs143", "", "CS143");
+  $result = $mysqli->query($query);
+  //$row = $result->fetch_assoc();
+  //echo $row;
   //print_r($row);
-  foreach($row as $val)
-  {
-    echo $val.' ';
+  if(!$result){
+    echo 'Check your syntax!<br>Note that we do not support multiple queries.<br>';
   }
-  echo '<br>';
+  else{
+    //echo '<table>';
+?>
+<table border='1'>
+<?php
+    $c = 0;
+    while($row = $result->fetch_assoc())
+    {
+      if($c==0){
+        echo '<tr>';
+        foreach($row as $field=>$val)
+        {
+          echo '<td style="font-weight:bold">'.$field.'</td>';
+        }
+        echo '</tr>';
+      }
+      echo '<tr>';
+      foreach($row as $field=>$val)
+      {
+        echo '<td>'.$val.'</td>';
+      }
+      echo '</tr>';
+      $c = $c+1;
+    }
+    //echo '</table>';
+?>
+</table>
+<?php
+  }
+}catch(Exception $e) {
+    echo 'Error: ',  $e->getMessage(), "\n";
 }
-//$row = $result->fetch_assoc();
-//echo htmlentities($row);
-//echo $query;
 
 ?>
 
