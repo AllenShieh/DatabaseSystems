f582361c7055b6736594544cbac6b8380c1d5a28
AllenShieh <yyy88xxx@163.com>
2018-05-31 14:01:14 -0700

    countvectorize

--- P2B/reddit_model_v2.py
+++ P2B/reddit_model_v2.py
@@ -4,19 +4,27 @@ from pyspark.sql import SQLContext
 
 # IMPORT OTHER MODULES HERE
 flag = 0
+from pyspark.sql.functions import udf
+from pyspark.sql.types import *
+from pyspark.ml.feature import CountVectorizer
 
-def main(context):
+def join(comments, label):
+	# task 2
+	associated = comments.join(label, comments.id == label.Input_id, 'inner')
+	return associated.select("id","body","labeldem","labelgop","labeldjt")
+
+def makeNgrams(text):
+    ngrams = cleantext.sanitize(text)
+    concat = ngrams[1]+' '+ngrams[2]+' '+ngrams[3]
+    return concat.split(' ')
+
+makeNgrams_udf = udf(makeNgrams, ArrayType(StringType()))
+
+def main(sqlContext):
     """Main function takes a Spark SQL context."""
     # YOUR CODE HERE
     # YOU MAY ADD OTHER FUNCTIONS AS NEEDED
-
-if __name__ == "__main__":
-    conf = SparkConf().setAppName("CS143 Project 2B")
-    conf = conf.setMaster("local[*]")
-    sc   = SparkContext(conf=conf)
-    sqlContext = SQLContext(sc)
-    sc.addPyFile("cleantext.py")
-    # load csv file
+    label = sqlContext.read.load("labeled_data.csv",format="csv", sep=",", inferSchema="true", header="true")
     if(flag):
         comments = sqlContext.read.json("comments-minimal.json.bz2")
         submissions = sqlContext.read.json("submissions.json.bz2")
@@ -28,4 +36,22 @@ if __name__ == "__main__":
         comments = sqlContext.read.parquet("comments_data")
         submissions = sqlContext.read.parquet("submissions_data")
         print("loading done")
+    associated = join(comments, label)
+    withngrams = associated.withColumn("ngrams", makeNgrams_udf(associated['body']))
+    # comments.show()
+    # associated.show()
+    # withngrams.show()
+    cv = CountVectorizer(binary=True, inputCol="ngrams", outputCol="vectors")
+    model = cv.fit(withngrams)
+    model.transform(withngrams).show()
+
+if __name__ == "__main__":
+    conf = SparkConf().setAppName("CS143 Project 2B")
+    conf = conf.setMaster("local[*]")
+    sc   = SparkContext(conf=conf)
+    sqlContext = SQLContext(sc)
+    sc.addPyFile("cleantext.py")
+    import cleantext
+    # load csv file
+
     main(sqlContext)
